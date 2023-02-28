370fa772fa6ef4662dda57e27ddd06811a4501fb
avnish <avnishbdp@gmail.com>
2023-02-28 19:19:49 +0530

    single

---
+++ frontend/src/Components/SizeProduct.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import { FaCheck } from 'react-icons/fa';
+import '../CSS/SingleProduct.css'
+import styled from 'styled-components';
+const SizeProduct = ({size}) => {
+    const [color, setColor] = useState(size[0]);
+  return (
+    <Wrapper>
+    <div className='size'>
+    <p>
+            Colors:
+            {
+                size.map((curColor,index)=>{
+                    return (
+                        <button
+                key={index}
+                style={{ backgroundColor: curColor }}
+                className={color === curColor ? "btnStyle active" : "btnStyle"}
+                onClick={() => setColor(curColor)}>
+                {color === curColor ? <FaCheck className="checkStyle" /> : null}
+              </button>
+                    )
+                   
+                })
+            }
+        </p>
+    </div>
+    </Wrapper>
+  )
+}
+const Wrapper = styled.section`
+  .colors p {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+  }
+  .btnStyle {
+    width: 2rem;
+    height: 2rem;
+    background-color: #000;
+    border-radius: 50%;
+    margin-left: 1rem;
+    border: none;
+    outline: none;
+    opacity: 0.5;
+    cursor: pointer;
+    &:hover {
+      opacity: 1;
+    }
+  }
+  .active {
+    opacity: 1;
+  }
+  .checkStyle {
+    font-size: 1.4rem;
+    color: #fff;
+    margin-left: 3px;
+  }
+`;
+export default SizeProduct
--- frontend/src/Pages/SingleProduct.jsx
+++ frontend/src/Pages/SingleProduct.jsx
@@ -1,75 +1,299 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
-import '../CSS/SingleProduct.css'
+import "../CSS/SingleProduct.css";
+import { FaFacebook, FaStar, FaStarHalfAlt, FaTwitter } from "react-icons/fa";
+import {
+  AiOutlineHeart,
+  AiOutlineShoppingCart,
+  AiOutlineStar,
+} from "react-icons/ai";
+import { TbTruckDelivery } from "react-icons/tb";
+import { BiDetail, BiPurchaseTag } from "react-icons/bi";
+import axios from 'axios'
 
-import { Box, Button, Image, SimpleGrid, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  HStack,
+  Image,
+  Input,
+  InputGroup,
+  InputRightElement,
+  ListItem,
+  SimpleGrid,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+  UnorderedList,
+} from "@chakra-ui/react";
+import SizeProduct from "../Components/SizeProduct";
 const SingleProduct = () => {
+  const {id}= useParams()
+  const [single, setSingle]= useState({})
+  useEffect(()=>{
+    axios.get(`http://localhost:8080/Winter/${id}`)
+    .then((r)=>{
+        setSingle({...r.data})
+      })
+      .catch((e)=>{
+      })
+  },[setSingle])
 
-  // const {id} = useParams();
-  // const SingleProduct= useSelector((store)=>store.winterdata)
-  
-  // let data= SingleProduct.find((el)=>el.id)
+const {product_img}= single;
+
+
+console.log(product_img)
+  const size = ["#ff755a", "#aecfed", "#0000ff", "#e42529"];
   const imgs = [
     "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/10673544/2019/9/24/6b9c7688-7ca2-4d11-9e5b-a3745ecd8f761569310358973-The-Indian-Garage-Co-Men-Shirts-8481569310357131-1.jpg",
     "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/10673544/2019/9/24/729859d8-cc66-4465-ba81-27028b9d7a461569310358945-The-Indian-Garage-Co-Men-Shirts-8481569310357131-2.jpg",
     "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/10673544/2019/9/24/66eb9c68-a2d9-41b1-8016-cf769e78d3a11569310358918-The-Indian-Garage-Co-Men-Shirts-8481569310357131-3.jpg",
-    "https://cf-images.ap-southeast-1.prod.boltdns.net/v1/jit/5745608584001/fa2bf46e-f698-433e-b18a-e492e6d9dbc3/main/1280x720/21s56ms/match/image.jpg"
-  ]
+    "https://cf-images.ap-southeast-1.prod.boltdns.net/v1/jit/5745608584001/fa2bf46e-f698-433e-b18a-e492e6d9dbc3/main/1280x720/21s56ms/match/image.jpg",
+  ];
+  
   const [mainImage, setMainImage] = useState(imgs[0]);
+  let reviews = single.rewiew;
+
+  const postCart  = ()=>{
+    fetch("http://localhost:8080/cart", {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+  
+      body:JSON.stringify(single)
+    }).then((resp)=>{
+      
+      resp.json().then((result)=>{
+       
+      })
+    })
+  }
+  
 
   
-  console.log(mainImage)
-  return (
-    <Box >
+  const ratingStar = Array.from({ length: 5 }, (elem, index) => {
+    let number = index + 0.5;
+    let stars = single.rating;
+
+
+
+
     
-    <SimpleGrid columns={[1,1,2,2]}>
-      <Box border={'1px solid red'}   >
-      <Image borderRadius={10} src={mainImage} w={250} m={'auto'} mt={5}/>
-      <SimpleGrid columns={[2,4,4,4]} gap={4} p={5} m={'auto'}>
-       {
-        imgs.map((curLem,index)=>{
-          return (
-            <Image cursor={'pointer'} w={130} borderRadius={10} src={curLem} onClick={() => setMainImage(curLem)}/>
-          )
-        })
-       }
-        </SimpleGrid>
-      </Box>
+    return (
+      <span key={index}>
+        {stars >= index + 1 ? (
+          <FaStar className="icon" />
+        ) : stars >= number ? (
+          <FaStarHalfAlt className="icon" />
+        ) : (
+          <AiOutlineStar className="icon" />
+        )}
+      </span>
+    );
+  });
+
+
+  return (
+    <Box>
+      <SimpleGrid columns={[1, 1, 2, 2]} p={5}>
+        <Box>
+          <Image borderRadius={10} src={mainImage} w={250} m={"auto"} mt={5} />
+          <SimpleGrid columns={[2, 4, 4, 4]} gap={4} p={5} m={"auto"}>
+            {imgs.map((curLem, index) => {
+              return (
+                <Image
+                  key={index}
+                  cursor={"pointer"}
+                  w={130}
+                  borderRadius={10} 
+                  src={curLem}
+                  onClick={() => setMainImage(curLem)}
+                />
+              );
+            })}
+            
+          </SimpleGrid>
+        </Box>
+
+        <Box p={5} w={"90%"} border={"0px solid red"}>
+          <Text fontWeight={"bold"} fontSize={"xl"}>
+            {single.title}
+          </Text>
+          <Text fontSize={"xl"}>{single.titledetails}</Text>
+         
+         
+          <Box display={"flex"} mt={5} mb={2}>
+            {ratingStar}
+            <Text ml={4}>({reviews} customer reviews)</Text>
+          </Box>
+          <hr />
+
+          <Text mt={3} fontWeight={"bold"} fontSize={"sl"}>
+            ₹{single.price} MRP
+            <Text as="del" fontWeight={"400"} ml={1} mr={1}>
+              ₹1699
+            </Text>{" "}
+            <span>(56% OFF)</span>
+          </Text>
+          <Text color="teal" fontWeight={"500"} mt={2} mb={2}>
+            inclusive of all taxes
+          </Text>
+          <Text fontWeight={"bold"} mb={5}>
+            SELECT COLOR
+          </Text>
+          <SizeProduct size={size} />
 
 
 
-      <Box border={'1px solid red'}>
-        <Text>Carlton London sports</Text>
-        <Text>Women Olive Green T-Strap Flats</Text>
-        <Text>Women Olive Green T-Strap Flats</Text>
-        <hr/> 
-        <Text>₹747  MRP ₹1699  (56% OFF)</Text>
-        <span>inclusive of all taxes</span>
-        <Text>SELECT SIZE (Euro Size)</Text>
-        <Button >ADD TO BAG</Button> 
-        <Button ml={2}>WISHLIST</Button>
-        <hr/>
-      </Box>
-    </SimpleGrid>
+          <HStack mt={7} mb={5}>
+            <Button colorScheme="red" leftIcon={<AiOutlineShoppingCart />} onClick={postCart}>
+              ADD TO BAG
+            </Button>
+            <Button colorScheme="teal" leftIcon={<AiOutlineHeart />}>
+              WISHLIST
+            </Button>
+          </HStack>
+          <hr />
+
+
+          <Box display={"flex"} mt={4}>
+            <Text fontWeight={"500"} mr={3}>
+              DELIVERY OPTIONS
+            </Text>
+            <Text mt={1}>
+              <TbTruckDelivery />
+            </Text>
+          </Box>
+
+          <InputGroup size="md" w={"15rem"} mt={5}>
+            <Input />
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="sm" bg={"white"} color={"pink.500"}>
+                Check
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+
+
+          
+          <Text mt={3} fontSize={"13px"}>
+            Please enter PIN code to check delivery time & Pay on Delivery
+            Availability
+          </Text>
+
+          <Text mt={5}>Try & Buy might be available</Text>
+          <Text mt={2}>100% Original Products</Text>
+          <Text mt={2}>Pay on delivery might be available</Text>
+          <Text mt={2}>Easy 30 days returns and exchanges</Text>
+
+          <Box display={"flex"} mt={4}>
+            <Text fontWeight={"bold"} mr={3}>
+              BEST OFFERS{" "}
+            </Text>
+            <Text mt={1}>
+              <BiPurchaseTag className="dil-font" />
+            </Text>
+          </Box>
+
+          <Box display={"flex"} mt={4}>
+            <Text fontWeight={"bold"}>Best Price:</Text>
+            <Text ml={2} fontWeight={"bold"} color={"orange"}>
+              Rs. {single.price-200}
+            </Text>
+          </Box>
+          <UnorderedList mt={4}>
+            <ListItem>Coupon code: MYNTRA200</ListItem>
+            <ListItem>
+              Coupon Discount: Rs. 175 off (check cart for final savings)
+            </ListItem>
+            <ListItem>
+              Applicable on: Orders above Rs. 799 (only on first purchase)
+            </ListItem>
+          </UnorderedList>
+          <Text
+            mb={6}
+            mt={3}
+            fontSize={"13px"}
+            fontWeight={"bold"}
+            color={"pink.500"}
+          >
+            View Eligible Products
+          </Text>
+          <hr />
+
+          <Box display={"flex"} mt={4}>
+            <Text fontWeight={"bold"} mr={3}>
+              PRODUCT DETAILS{" "}
+            </Text>
+            <Text mt={1}>
+              <BiDetail className="dil-font" />
+            </Text>
+          </Box>
+
+
+
+          
+          <Text mt={4}>Colour: maroon</Text>
+          <Text>Ethnic motifs printed</Text>
+          <Text>Round neck</Text>
+          <Text>Three-quarter, regular sleeves</Text>
+          <Text>Straight shape with regular style</Text>
+          <Text>Thread work detail</Text>
+          <Text>Calf length with straight hem</Text>
+          <Text>Machine weave regular silk</Text>
+
+          <Text fontWeight={"bold"} mt={3} mb={3}>
+            Size & Fit
+          </Text>
+          <Text>The model (height 5'8) is wearing a size S</Text>
+          <Text fontWeight={"bold"} mt={3} mb={2}>
+            Material & Care
+          </Text>
+          <Text>100% VISCOSE MACHIN WASH</Text>
+          <Text fontWeight={"bold"} mt={3}>
+            Specifications
+          </Text>
+
+          <TableContainer mt={5} w={"80%"}>
+            <Table size="sm">
+              <Thead>
+                <Tr>
+                  <Th>Sleeve Length</Th>
+                  <Th>Shape</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>Three-Quarter Sleeves</Td>
+                  <Td>Straight</Td>
+                </Tr>
+                <Tr>
+                  <Td>Round Neck</Td>
+                  <Td>Ethnic Motifs </Td>
+                </Tr>
+                <Tr>
+                  <Td>Regular</Td>
+                  <Td>Side Slits</Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Box>
+
+
+      </SimpleGrid>
     </Box>
-  )
-}
+  );
+};
 
-export default SingleProduct
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default SingleProduct;
