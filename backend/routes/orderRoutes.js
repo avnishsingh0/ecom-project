6206da5e80057fc20cc4f00d8920d5e60a153040
avnish <avnishbdp@gmail.com>
2023-04-04 10:27:46 +0530

    backend

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,7 +3,6 @@ const { addOrederItem } = require("../controllers/orderController");
 const { protect } = require("../middlewares/authMiddlware");
 const router = express.Router();
 
-
 router.route("/").post(addOrederItem)
 
 module.exports = router;
